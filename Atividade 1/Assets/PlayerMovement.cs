d3655c9538153fbc0ae21ed6fd076f2098268edc
LucasGazetta <Lucas_Gazetta@users.noreply.github.com>
2019-03-24 20:14:14 -0300

    Tentativa Codigo de Mov 1

---
+++ Atividade 1/Assets/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    public float horizontalMove = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+       horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        // move o personagem
+        controller.Move(move: horizontalMove * Time.fixedDeltaTime, crouch: false, jump: false);
+    }
+}
